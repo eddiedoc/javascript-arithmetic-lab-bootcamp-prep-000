e90da8f8ff34413c56ae2d437d5536e82f904fbd
eddie <psyck_o@hotmail.com>
2017-12-30 09:05:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function add() {
+  return 1 + 80; //81
+}
+
+function subtract() {
+  return 60 - 40; // 20
+}
+
+function multiply() {
+  return 2 * 3.4; // 6.8
+}
+
+function  divide() {
+  return 5.0 / 2.5; // 2
+}
